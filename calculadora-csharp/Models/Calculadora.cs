15407f3366117f565d8c0c9c51b695c715494c5e
unknown <johnferreira141@gmail.com>
2023-11-19 15:37:32 -0300

    Calculadora: Potencia e trigonometria basica

--- calculadora-csharp/Models/Calculadora.cs
+++ calculadora-csharp/Models/Calculadora.cs
@@ -26,5 +26,26 @@ namespace ExemploFundamentos.Models
             int result=x/y;
             System.Console.WriteLine($"{x}/{y}={result}");
         }
+
+        public void Pot(int x, int y){
+            double result=Math.Pow(x,y);
+            System.Console.WriteLine($"{x}^{y}={result}");
+        }
+
+        public void Seno(double ang){
+            double radiano=ang * Math.PI/100;
+            double seno=Math.Sin(radiano);
+            System.Console.WriteLine($"Seno de {ang} = {Math.Round(seno,4)}");
+        }
+        public void Cos(double ang){
+            double radiano=ang * Math.PI/100;
+            double cos=Math.Cos(radiano);
+            System.Console.WriteLine($"Coseno de {ang} = {Math.Round(cos,4)}");
+        }
+        public void Tang(double ang){
+            double radiano=ang * Math.PI/100;
+            double tan=Math.Tan(radiano);
+            System.Console.WriteLine($"Tangente de {ang} = {Math.Round(tan,4)}");
+        }
     }
 }
--- calculadora-csharp/Program.cs
+++ calculadora-csharp/Program.cs
@@ -6,3 +6,9 @@ calc.Sum(10, 5);
 calc.Sub(10, 8);
 calc.Mult(10, 2);
 calc.Div(500, 5);
+
+calc.Pot(3,3);
+
+calc.Seno(90);
+calc.Cos(90);
+calc.Tang(90);
