34e2499d4154526a582dc76a008c35bb1d586044
unknown <johnferreira141@gmail.com>
2023-11-23 15:48:54 -0300

    Redimensionando um array usando resize

--- arraysListas/Program.cs
+++ arraysListas/Program.cs
@@ -17,3 +17,20 @@ foreach (int val in arrayInt)//percorrendo array com foreach
     System.Console.WriteLine($"Posição:{countForeach} | Posição:{val}");
     countForeach++;
 }
+
+
+Array.Resize(ref arrayInt, arrayInt.Length * 2);//Cria um novo array usando o antigo como referencia e copia os elementos antigos para este novo array
+
+System.Console.WriteLine("Percorrendo array com for depois do resize");
+for(int i=0;i<arrayInt.Length;i++){//percorrendo array com for
+    System.Console.WriteLine($"Posição:{i} | Elemento:{arrayInt[i]}");
+}
+
+countForeach=0;
+System.Console.WriteLine("Percorrendo array com foreach depois do resize");
+foreach (int val in arrayInt)//percorrendo array com foreach
+{
+    System.Console.WriteLine($"Posição:{countForeach} | Posição:{val}");
+    countForeach++;
+}
+
