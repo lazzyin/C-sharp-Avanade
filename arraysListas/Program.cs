c8e1739d8671b95c8f3f32605f05052b6f42658b
unknown <johnferreira141@gmail.com>
2023-11-23 15:56:55 -0300

    Copiando o valor de um array para outro

--- arraysListas/Program.cs
+++ arraysListas/Program.cs
@@ -5,6 +5,8 @@ arrayInt[1]=7;
 arrayInt[2]=9;
 //arrayInt[3]=10; Indice invalido pois excede o tamanho do array
 
+
+//PERCORRENDO OS ELEMENTOS DE UM ARRAY
 System.Console.WriteLine("Percorrendo array com for");
 for(int i=0;i<arrayInt.Length;i++){//percorrendo array com for
     System.Console.WriteLine($"Posição:{i} | Elemento:{arrayInt[i]}");
@@ -18,6 +20,9 @@ foreach (int val in arrayInt)//percorrendo array com foreach
     countForeach++;
 }
 
+/*
+
+//REDIMENSIONANDO ARRAY USANDO RESIZE
 
 Array.Resize(ref arrayInt, arrayInt.Length * 2);//Cria um novo array usando o antigo como referencia e copia os elementos antigos para este novo array
 
@@ -33,4 +38,18 @@ foreach (int val in arrayInt)//percorrendo array com foreach
     System.Console.WriteLine($"Posição:{countForeach} | Posição:{val}");
     countForeach++;
 }
+*/
 
+/*
+COPIANDO OS ELEMENTOS DE UM ARRAY PARA OUTRO
+*/
+
+int[] arrayIntDobrado = new int[arrayInt.Length * 2];//Cria um novo array com o dobro da capacidade do arrayInt
+Array.Copy(arrayInt, arrayIntDobrado,arrayInt.Length);//Copia todos os elementos do arrayInt e passa para o novo array
+
+System.Console.WriteLine("Copia do array:");
+foreach (int val in arrayIntDobrado)//percorrendo array com foreach
+{
+    System.Console.WriteLine($"Posição:{countForeach} | Posição:{val}");//Por não terem sido declarados os elementos a mais do novo array terão o valor de 0
+    countForeach++;
+}
